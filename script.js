9ede158894fd039e78559d6d9ab6d18da7cb8aab
md1300 <md.tawhid.khan1998@gmail.com>
2024-05-22 16:10:06 +0600

    fourth commit

--- script.js
+++ script.js
@@ -2,35 +2,35 @@
 // .then(res=>res.json())
 // .then(data=>console.log(data))
 
-const discussSession=async()=>{
-    const res=await fetch(' https://openapi.programming-hero.com/api/retro-forum/posts?category=coding')
+const discussSession=async(categoryName)=>{
+    const res=await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${categoryName}`)
     const data=await res.json();
     
-    const forum=data.posts[0];
-    console.log(forum);
+    const forum=data.posts;
+//    console.log(forum);
     display(forum)
 };
 
-discussSession();
-const display=(forums)=>{
-    console.log('what are you waiting for',forums);
+
+const display=(posts)=>{
+    posts.forEach((post)=>{
     const kidsCard=document.getElementById('kids-card')
     console.log(kidsCard);
     const newKidsCard=document.createElement('div');
     newKidsCard.innerHTML=`
     <section class="flex justify-between justify-items-center items-center">
     <div>
-    <img src="${forums.image}" alt="" srcset="">
+    <img src="${post.image}" alt="" srcset="">
     </div>
     <div>
     <div class="items-center">
     <div class="flex "> 
-    <h5 class="mx-5"># ${forums.category}</h5>
-    <h5>author: ${forums.author.name}</h5>
+    <h5 class="mx-5"># ${post.category}</h5>
+    <h5>author: ${post.author.name}</h5>
     </div>
     <div>
-    <h3>${forums.title}</h3>
-    <p>${forums.description}</p>
+    <h3>${post.title}</h3>
+    <p>${post.description}</p>
     </div>
     </div>
     
@@ -39,15 +39,15 @@ const display=(forums)=>{
     <div class=" flex justify-around ">
     <div class= "flex justify-between items-center">
     <img src="images/Group 13.png" alt="" srcset="">
-    <p>${forums.comment_count }</p>
+    <p>${post.comment_count }</p>
     </div>
     <div  class= "flex justify-between items-center">
      <img src="images/Group 16.png" alt="" srcset="">
-    <p>${forums. view_count}</p>
+    <p>${post. view_count}</p>
     </div>
     <div  class= "flex justify-between items-center">
     <img src="images/Group 18.png" alt="" srcset="">
-    <p>${forums. posted_time}</p>
+    <p>${post. posted_time}</p>
     </div>
     </div>
     <div>
@@ -57,7 +57,66 @@ const display=(forums)=>{
     </div>
     </section>
 
-    <h3>${forums.isActive}</h3>
+    <h3>${post.isActive}</h3>
     `;
     kidsCard.appendChild(newKidsCard) ;
+   });
+    
+}
+
+const searchButton=()=>{
+    const searchField=document.getElementById('search-field');
+    const searchText=searchField.value;
+    console.log(searchText);
+    discussSession(searchText)
+
+}
+
+discussSession();
+
+//------------------ latest post -----------------------
+const latestPost=async()=>{
+    const res=await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+    const posts= await res.json()
+    // console.log(posts);
+    displayPost(posts);
 };
+const displayPost=(posts)=>{
+//    console.log(posts)
+const updatePost=document.getElementById('latest-post');
+
+posts.forEach(post=>{
+    console.log(post)
+    
+    // console.log(updatePost);
+    const postCard=document.createElement('div');
+     postCard.innerHTML=`
+     <img src="${post.cover_image}" alt="" srcset="">
+     <div class="flex">
+     <img src="images/Frame (1).png" alt="" srcset="">
+     <p>${post.author.posted_date}</p>
+     </div>
+     <h4>${post.title}</h4>
+     <p>${post.description}</p>
+     <div class="flex justify-between items-center ">
+     <div>
+     <img src="${post.profile_image}" alt="" srcset="">
+     </div>
+     <div>
+     <h4>${post.author.name}</h4>
+     <p>${post.author.designation}</p>
+     </div>
+     </div>
+     `
+     updatePost.appendChild(postCard);
+})
+}
+latestPost();
+
+
+
+
+
+
+
+
